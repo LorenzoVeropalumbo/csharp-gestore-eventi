ed467101c1cdec92307b20a5db6aff9ab9a2d0e7
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-11-10 14:57:00 +0100

    creazione report errore

--- GestioneFile.cs
+++ GestioneFile.cs
@@ -18,43 +18,83 @@ static class GestioneFile
 
     public static void ImportFile(string Filename, ProgrammaEventi programmaEventi)
     {
+        StreamWriter fileDaScrivere = File.CreateText("C:\\Users\\loren\\source\\repos\\.Net-course\\csharp-gestore-eventi\\report.csv");
         StreamReader stream = File.OpenText("C:\\Users\\loren\\source\\repos\\.Net-course\\csharp-gestore-eventi\\ListaEventi.csv");
+        
         stream.ReadLine();
         while (!stream.EndOfStream)
         {
+            bool IsError = false;
             string line = stream.ReadLine();
             string[] info = line.Split(",");
+            string[] errori = new string[info.Length];
+            string errore = "";
             if (info.Length == 4)
             {
+                string titolo = "";
                 if (info[0] == "")
                 {
-                    return;
+                    IsError = true;
+                    errore += "hai inserito un titolo sbagliato - ";
+                    errori[0] = info[0];
                 }
-                string titolo = info[0];              
+                else
+                {
+                    titolo = info[0];
+                }
 
+                DateTime data = new DateTime();
                 if (!DateTime.TryParse(info[1], out DateTime time))
                 {
-                    return;
+                    IsError = true;
+                    errore += "hai inserito una data sbagliata - ";
+                    errori[1] = info[1];
                 }
-                DateTime data = Convert.ToDateTime(info[1]);
+                else 
+                {
+                    data = Convert.ToDateTime(info[1]);
+                }
 
+                int posti = 0;
                 if (!Int32.TryParse(info[2], out int post))
                 {
-                    return;
+                    IsError = true;
+                    errore += "hai inserito un numero posti sbagliato - ";
+                    errori[2] = info[2];
                 }
-                int posti = Convert.ToInt32(info[2]);
-                
+                else
+                {
+                    posti = Convert.ToInt32(info[2]);
+                }
+
+                int postiPrenotati = 0;
                 if (!Int32.TryParse(info[3], out int postpre))
                 {
-                    return;
+                    IsError = true;
+                    errore += "hai inserito un numero posti penotati sbagliato - ";
+                    errori[3] = info[3];
                 }
-                int postiPrenotati = Convert.ToInt32(info[3]);
+                else
+                {
+                    postiPrenotati = Convert.ToInt32(info[3]);       
+                }
                 
-                Evento evento = new Evento(titolo, data, posti);
-                evento.PrenotaPosti(postiPrenotati);
-                programmaEventi.AggiungiEvento(evento);
+                
+                if (IsError)
+                {
+                    fileDaScrivere.WriteLine("questo evento è sbagliato ->\n" +
+                        "\t Titolo: {0} \n" + "\t Data: {1} \n" + "\t Numero Posti: {2} \n" + "\t Numero Posti Prenotati: {3} \n" + "Errore: {4}", errori[0], errori[1], errori[2], errori[3], errore);
+                }
+                else
+                {
+                    Evento evento = new Evento(titolo, data, posti);
+                    evento.PrenotaPosti(postiPrenotati);
+                    programmaEventi.AggiungiEvento(evento);
+                }
+
             }
         }
         stream.Close();
+        fileDaScrivere.Close();
     }
 }
